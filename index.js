583cb47b32a49be50614d54415ba3e240eea08ec
Renée Cruz <reneebasscruz@gmail.com>
2019-02-03 08:21:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   }
   else 
-    if (string === "I love you, Grandma."){
+     (string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
