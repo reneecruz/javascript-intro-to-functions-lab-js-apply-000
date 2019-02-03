28d304c4ad08790f36ff2fafc4d98d27de9a7c32
Renée Cruz <reneebasscruz@gmail.com>
2019-02-03 08:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function whisper(string) {
 function sayHiToGrandma(string) {
   
   if (string.toLowerCase() === lowercase) {
-  return('I can\'t hear you!');
+  return('I can\'t hear you!')
   }
   else if (string.toUpperCase() === uppercase) {
   return("YES INDEED!");
