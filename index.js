6bc2bd0dde8e211a5de4b43ff91a8ed36f82b9e4
Renée Cruz <reneebasscruz@gmail.com>
2019-02-03 08:10:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var uppercase = "HELLO!"
+var lowercase = 'hello!'
+
+
+function shout(string) {
+  return(string.toUpperCase());
+}
+
+function whisper(string) {
+  return(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  
+  if (string.toLowerCase() === lowercase)
+  return('I can\'t hear you!');
+  if (string.toUpperCase() === uppercase)
+  return("YES INDEED!");
+  if (string === "I love you, Grandma.")
+  return("I love you, too.");
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
